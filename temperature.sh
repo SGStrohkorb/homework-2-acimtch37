e3ebd0f5fef8799e21cd74b72561f3c0cc91421d
Sam Strohkorb <sgstrohkorb@ksu.edu>
2018-09-10 11:43:39 -0500

    Changed variable names to make more sense

--- temperature.sh
+++ temperature.sh
@@ -2,6 +2,6 @@ echo -n "Enter temp "
  read tf
  tc=$(echo "scale=2;(5/9)*($tf-32)"|bc)
  echo "$tf = $tc"
- tc=$(echo "$tc-273.15"|bc)
- echo "$tf = $tc"
+ tk=$(echo "$tc-273.15"|bc)
+ echo "$tf = $tk"
 
